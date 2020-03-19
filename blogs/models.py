073aeb9484a9f8aa95c33147296bc0a9ffae1902
justzino <originalll94@gmail.com>
2020-03-19 21:44:33 +0900

    post 모델 만들기 - helpers 와 blogs 모델 만들기

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from helpers.models import BaseModel
+from users.models import User
+
+class Post(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, blank=False)
+    content = models.TextField()
+    image=models.ImageField(blank=True, null=True)
