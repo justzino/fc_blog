0317c19e96153a3d924d6469fb0128636799efb2
justzino <originalll94@gmail.com>
2020-03-19 19:40:36 +0900

    user 모델에 성별 추가, login 생성. But, superuser생성 안되는중..

--- users/models.py
+++ users/models.py
@@ -35,7 +35,7 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, 'blogs/like_section.html',  password, **extra_fields)
+        return self._create_user(email, '',  password, **extra_fields)
 
 
 class User(AbstractUser):
