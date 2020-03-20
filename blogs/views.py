06f09a9b2f231872f8ce00e7ef0dc49dc5ac6301
justzino <originalll94@gmail.com>
2020-03-20 22:15:39 +0900

    blogs post view를 최신순부터 게시물 정렬

--- blogs/views.py
+++ blogs/views.py
@@ -4,6 +4,6 @@ from blogs.models import Post
 
 
 def posts_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-created_at')    # 최신순부터 게시물 정렬
 
     return render(request, 'blogs/posts_list.html', context={'posts':posts})
